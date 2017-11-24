30637ca9c7318806bd6c45760d10b7bcf4b7ceeb
sColin16 <csil167593@adams12.org>
2017-11-24 12:31:32 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var para = document.createElement("p");
+var node = document.createTextNode("Javascript running");
+para.appendChild(node);
+
+var element = document.querySelector("body").appendChild(para);
